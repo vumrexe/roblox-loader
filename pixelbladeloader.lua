9e388ff9e62c6311e84552c38403cdbaba81e910
vumrexe <makalakalakaka@gmail.com>
2026-03-20 07:51:22 +0100

    pixelbladeloader.lua

--- pixelbladeloader.lua
+++ pixelbladeloader.lua
@@ -4,13 +4,4 @@ if queue_on_teleport then
     queue_on_teleport(loader)
 end
 
--- Auto Rejoin Logic
-local GuiService = game:GetService("GuiService")
-local TeleportService = game:GetService("TeleportService")
-
-GuiService.ErrorMessageChanged:Connect(function()
-    task.wait(5)
-    TeleportService:Teleport(game.PlaceId, game.Players.LocalPlayer)
-end)
-
 loadstring(game:HttpGet("https://qweenhub.netlify.app/loader/pixelblade.lua"))()
